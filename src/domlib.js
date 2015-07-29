cf305c2dad88f95b2a22af374cc6013bd6b95dbf
Adam Haile <aah@curveship.org>
2015-07-28 21:40:49 -0400

    domlib: cross-platform framework; supporting for testing if a node is content editable or not

--- src/domlib.js
+++ src/domlib.js
@@ -1,6 +1,7 @@
 // internal cross-browser library of required DOM functions
 define('domlib', [], function () {
-    return {
+    // default (conformant) implementations
+    var domlib = {
         addEventListener: function addEventListener(node, event, fn) {
             node.addEventListener(event, fn, false);
         },
@@ -19,6 +20,34 @@ define('domlib', [], function () {
 
         classListRemove: function (el, name) {
             return el.classList.remove(name);
+        },
+        
+        isContentEditable: function (el) {
+            return el.isContentEditable;
+        },
+        
+        isAttachedToDocument: function (el) {
+            return (document.compareDocumentPosition(el) & 1) === 0;
         }
     };
+    
+    // shims for broken and/or older browsers
+    if (!browserSetsIsContentEditablePropertyReliably())
+        useContentEditableAttribute();
+    
+    return domlib;
+    
+    // Element.isContentEditable is currently broken on Chrome.  It returns false for non-displayed elements. See https://code.google.com/p/chromium/issues/detail?id=313082 .
+    function browserSetsIsContentEditablePropertyReliably() {
+        var div = document.createElement("div");
+        div.innerHTML = '<div contentEditable="true"></div>';
+        return div.children[0].isContentEditable === true;
+    }
+    
+    function useContentEditableAttribute() {
+        domlib.isContentEditable = function (el) {
+            var contentEditable = el.getAttribute("contentEditable");
+            return contentEditable === "true" || contentEditable === "";
+        }
+    }
 });
